ca0229f4aa2384d0c2bc4b747e03868842443674
Daniel Cabral <danielccabral1981@gmail.com>
2021-02-16 14:17:53 -0300

    Adicionando dados a sessão e a funcinalidade logout

--- jogoteca.py
+++ jogoteca.py
@@ -54,4 +54,11 @@ def autenticar():
         return redirect('/login')
 
 
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect('/')
+
+
 app.run(debug=True)  # reload na aplicação com o debug.
